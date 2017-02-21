eaccc4b1ee3e52bba82db9707b8f69b8cac81c8a
Brian <br01805@georgiasouthern.edu>
2017-02-21 16:39:08 -0500

    Latitude and longitude displaying for the map

--- app/src/main/java/com/example/brianr/rsslocation/MainActivity.java
+++ app/src/main/java/com/example/brianr/rsslocation/MainActivity.java
@@ -10,6 +10,7 @@ import android.app.Dialog;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.*;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.*;
@@ -65,8 +66,58 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mGoogleMap.setMyLocationEnabled(true);
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        mGoogleMap.addMarker(new MarkerOptions().position(MAVELIKARA).title("Marker"));
+        mGoogleMap.addMarker(new MarkerOptions().position(MAVELIKARA)
+                .title("Marker")
+                .draggable(true)
+                .snippet("Hello")
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
         mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(MAVELIKARA, 13));
+        mGoogleMap.setOnMarkerDragListener(new OnMarkerDragListener() {
+
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+                // TODO Auto-generated method stub
+                // Here your code
+                Toast.makeText(MainActivity.this, "Dragging Start",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                // TODO Auto-generated method stub
+                LatLng position = marker.getPosition(); //
+                Toast.makeText(
+                        MainActivity.this,
+                        "Lat: " + position.latitude + " \n"
+                                + "Long: " + position.longitude,
+                        Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+                // TODO Auto-generated method stub
+                // Toast.makeText(MainActivity.this, "Dragging",
+                // Toast.LENGTH_SHORT).show();
+                System.out.println("Draagging");
+            }
+        });
 
     }
+    public boolean onMarkerClick(final Marker marker) {
+
+        if (marker.equals(mGoogleMap)) {
+            // handle click here
+            // map.getMyLocation();
+            System.out.println("Clicked");
+            double lat = mGoogleMap.getMyLocation().getLatitude();
+            System.out.println("Lat" + lat);
+            Toast.makeText(MainActivity.this,
+                    "Current location " + mGoogleMap.getMyLocation().getLatitude(),
+                    Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+
 }
